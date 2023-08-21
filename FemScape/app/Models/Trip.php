5e640f60ff74c4c68cc601b9ac055aa5253c5244
leandrita <lea.bujh@gmail.com>
2023-08-21 07:56:27 -0300

    add database and Trip Model

---
+++ FemScape/app/Models/Trip.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model
+{
+
+    use HasFactory;
+
+    protected $fillable = [
+        'place',
+        'country',
+        'description',
+        'image',
+    ];
+
+    protected $hidden = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+}
