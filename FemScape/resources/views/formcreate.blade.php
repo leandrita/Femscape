cbb75690b2e4d82626284e08f2fb18960901999d
LucíaSilva <coderalsc@gmail.com>
2023-08-22 12:21:06 +0200

    Navbar

--- FemScape/resources/views/formcreate.blade.php
+++ FemScape/resources/views/formcreate.blade.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Formulario para crear destino</title>
+@extends('layouts.app')
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{ asset('css/formedit.css') }}">
-</head>
-<body>
-    <header></header>
+@section('connected')
+    <a href=""><img src="{{ asset('assets/images/Create-icon.svg') }}" class="navbar-icons" alt=""></a>
+    <a href=""><img src="{{ asset('assets/images/Logout-icon.svg') }}" class="navbar-icons logout" alt=""></a>
+@endsection
+
+        <link rel="stylesheet" href="{{ asset('css/formedit.css') }}">
+@section('content')
     <main class="container mt-5">
         <div class="row">
             <div class="liner"></div>
@@ -48,10 +44,4 @@
             </div>
         </div>
     </main>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="{{ asset('javascript/selectubication.js') }}"></script>
-</body>
-</html>
+@endsection
