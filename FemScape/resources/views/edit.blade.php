c998d81ed79bff056453ab8d282753a50ffc3f79
leandrita <lea.bujh@gmail.com>
2023-08-24 13:27:03 +0200

    add create form and show view styles

--- FemScape/resources/views/edit.blade.php
+++ FemScape/resources/views/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <main class="container mt-5 d-flex justify-content-center">
-                <form action="{{route('update', $trip)}}" method="POST" class="custom-form edit-form">
+                <form action="{{route('update', $trip)}}" method="POST" class="custom-form w-form">
                     @csrf
                     @method('PUT')
                     <h2 class="text-center mb-9 title">Editar destino</h2>
@@ -10,19 +10,19 @@
                     <div class="d-flex justify-content-around">
                         
                             <div class="form-group">
-                                <label for="title">Título</label>
+                                <label class="mt-4" for="title">Título</label>
                                 <input type="text" name="place" class="form-control" id="titulo" placeholder="Escribe un título" required value="{{$trip->place}}">
                                 <div class="help-block with-errors"></div>
-                                <label for="ubication">Ubicación</label>
+                                <label class="mt-4" for="ubication">Ubicación</label>
                                 <input class="form-control" name="country" id="ubicacion" placeholder="Escriba una ubicación" required value="{{$trip->country}}">
-                                <label for="ubication">Imagen</label>
+                                <label class="mt-4" for="ubication">Imagen</label>
                                 <input type="text" name="image" class="form-control" id="titulo" placeholder="Sube una imagen" required value="{{$trip->image}}">
-                            <button type="submit" class="btn btn-primary">Aceptar</button>
-                            <a href="{{ route('show', $trip->id) }}"><button type="button" class="btn btn-secondary">Cancelar</button></a>
+                            <button type="submit" class="btn btn-primary mt-6">Aceptar</button>
+                            <a href="{{ route('show', $trip->id) }}"><button type="button" class="btn btn-secondary mt-6">Cancelar</button></a>
                         </div>
 
                         <div class="col-md-6">
-                            <label for="description">¿Por qué quieres viajar allí?</label>
+                            <label class="mt-4" for="description">¿Por qué quieres viajar allí?</label>
                             <textarea class="form-control" name="description" id="comentarios" rows="12" required>{{$trip->description}}</textarea>
                         </div>
                     </div>
--- FemScape/resources/views/show.blade.php
+++ FemScape/resources/views/show.blade.php
@@ -3,18 +3,16 @@
 
 @section('content')
 
-<div class="container" style="margin-top: 4rem;">
-    <div class="row">
-        <div class="col-md-6">
-            <div class="d-flex justify-content-center align-items-center" style="margin-right: -10rem;">
-                <img class="rounded-4" src="{{ $trip->image }}" alt="{{ $trip->place }}" width="456" height="457">
-            </div>
+
+<main class="d-flex justify-content-center m-main">
+        <div class="d-flex justify-content-center align-items-center w-container">
+            <img class="rounded-4 w-img" src="{{ $trip->image }}" alt="{{ $trip->place }}">
         </div>
-        <div class="col-md-5 d-flex flex-column">
+        <div class="d-flex flex-column w-text">
             <div class="d-flex justify-content-between align-items-center">
-                <h3 style="color: #FF0060; font-weight: bold;">{{ $trip->place }}</h3>
+                <h3 class="place">{{ $trip->place }}</h3>
                 <div class="d-flex">
-                    
+                        
                     @auth
                     <a href="{{route('edit', $trip)}}"><img src="{{ asset('assets/images/Edit-icon.svg') }}" class="m-1" alt=""></a>
                     <form action="{{route('destroy', $trip)}}" method="POST">
@@ -25,16 +23,17 @@
                     @endauth
 
                     @guest
-                
+                    
                     @endguest
 
                 </div>
             </div>
-            <p style="color: #FF0060;">{{ $trip->country }}</p>
-            <p style="color: #0079FF; margin-top: 1rem;">{{ $trip->description }}</p>
+            <p class="country">{{ $trip->country }}</p>
+            <p class="description">{{ $trip->description }}</p>
         </div>
-    </div>
-</div>
+</main>
+   
+
 
 @endsection
 
