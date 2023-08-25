4ce77eb487061a97e5e0e4649ea55c1854e3b017
LucíaSilva <coderalsc@gmail.com>
2023-08-25 02:51:52 +0200

    adding test for page viw

---
+++ FemScape/tests/Feature/CreateTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateTest extends TestCase
+{
+    public function setUp(): void
+{
+    parent::setUp();
+
+    
+    $this->withoutMiddleware(\App\Http\Middleware\Authenticate::class);
+}
+
+    /**
+     * A basic feature test example.
+     */
+      public function test_create_view(): void
+    {
+        $this->withoutexceptionHandling();
+        $response = $this->get('/create');
+
+        $response->assertStatus(200);
+    }
+
+
+}
---
+++ FemScape/tests/Feature/ShowTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ShowTest extends TestCase
+{
+    public function setUp(): void
+    {
+    parent::setUp();
+
+
+    $this->withoutMiddleware(\App\Http\Middleware\Authenticate::class);
+    }
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_show_view(): void
+{
+    // Supongamos que el ID del elemento que deseas mostrar es 1
+    $itemId = 1;
+
+    $response = $this->get('/' . $itemId);
+
+    $response->assertStatus(200);
+}
+
+}
